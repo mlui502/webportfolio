4ff667ecb08a621600988ec095482654a2a84bbf
mlui502 <mlui502@aucklanduni.ac.nz>
2022-03-05 20:59:02 +1300

    About Placeholder
    Added Placeholder text for About page, Not Responsive

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 import Page from '../Page/Page';
+import styles from './About.module.css'
+import logo from '../../images/people.png';
 
 const About = () => {
 
     return(
         <Page>
+            <div className={styles.profile}>
+                <img src={logo} alt="Logo" className={styles.profileImage}/>   
 
+                <div className={styles.profileText}>
+                    <div className={styles.header}>
+                        Hi i'm Anton Lui
+                    </div>
+                    <p className={styles.text}>
+                        I am currently a Part III Software engineering student at The University of Auckland. Currently I am looking for software internships to develop my career
+                    </p>
+                    <p className={styles.text}>
+                        I would like to code something that will be used in the world. I am interested in all things software
+                    </p>
+                </div>
+            </div>
         </Page>
     );
 }
